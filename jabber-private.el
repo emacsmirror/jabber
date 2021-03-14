cdb4df5d22d415c9fc1acf90e0c819344c7738b3
contrapunctus <contrapunctus@disroot.org>
2021-03-14 08:57:29 +0530

    Move jabber-private code to jabber.org

--- jabber-private.el
+++ jabber-private.el
@@ -19,42 +19,6 @@
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
-;;;###autoload
-(defun jabber-private-get (jc node-name namespace success-callback error-callback)
-  "Retrieve an item from private XML storage.
-The item to retrieve is identified by NODE-NAME (a symbol) and
-NAMESPACE (a string).
-
-On success, SUCCESS-CALLBACK is called with JC and the retrieved
-XML fragment.
-
-On error, ERROR-CALLBACK is called with JC and the entire IQ
-result."
-  (jabber-send-iq jc nil "get"
-		  `(query ((xmlns . "jabber:iq:private"))
-			  (,node-name ((xmlns . ,namespace))))
-		  #'jabber-private-get-1 success-callback
-		  #'(lambda (jc xml-data error-callback)
-		      (funcall error-callback jc xml-data))
-		  error-callback))
-
-(defun jabber-private-get-1 (jc xml-data success-callback)
-  (funcall success-callback jc
-	   (car (jabber-xml-node-children
-		 (jabber-iq-query xml-data)))))
-
-;;;###autoload
-(defun jabber-private-set (jc fragment &optional 
-			      success-callback success-closure-data
-			      error-callback error-closure-data)
-  "Store FRAGMENT in private XML storage.
-SUCCESS-CALLBACK, SUCCESS-CLOSURE-DATA, ERROR-CALLBACK and
-ERROR-CLOSURE-DATA are used as in `jabber-send-iq'."
-  (jabber-send-iq jc nil "set"
-		  `(query ((xmlns . "jabber:iq:private"))
-			  ,fragment)
-		  success-callback success-closure-data
-		  error-callback error-closure-data))
 
 (provide 'jabber-private)
 
--- jabber.el
+++ jabber.el
@@ -7327,6 +7327,40 @@ on success or failure, respectively."
     (widget-value-set (cdr (assq 'bookmarks jabber-widget-alist)) value)
     (widget-setup)))
 
+;;;###autoload
+(defun jabber-private-get (jc node-name namespace success-callback error-callback)
+  "Retrieve an item from private XML storage.
+The item to retrieve is identified by NODE-NAME (a symbol) and
+NAMESPACE (a string).
+
+On success, SUCCESS-CALLBACK is called with JC and the retrieved
+XML fragment.
+
+On error, ERROR-CALLBACK is called with JC and the entire IQ
+result."
+  (jabber-send-iq jc nil "get"
+		  `(query ((xmlns . "jabber:iq:private"))
+			  (,node-name ((xmlns . ,namespace))))
+		  #'jabber-private-get-1 success-callback
+		  #'(lambda (jc xml-data error-callback)
+		      (funcall error-callback jc xml-data))
+		  error-callback))
+(defun jabber-private-get-1 (jc xml-data success-callback)
+  (funcall success-callback jc
+	   (car (jabber-xml-node-children
+		 (jabber-iq-query xml-data)))))
+;;;###autoload
+(defun jabber-private-set (jc fragment &optional
+			      success-callback success-closure-data
+			      error-callback error-closure-data)
+  "Store FRAGMENT in private XML storage.
+SUCCESS-CALLBACK, SUCCESS-CLOSURE-DATA, ERROR-CALLBACK and
+ERROR-CLOSURE-DATA are used as in `jabber-send-iq'."
+  (jabber-send-iq jc nil "set"
+		  `(query ((xmlns . "jabber:iq:private"))
+			  ,fragment)
+		  success-callback success-closure-data
+		  error-callback error-closure-data))
 (eval-when-compile (require 'cl))	;for ignore-errors
 ;; we need hexrgb-hsv-to-hex:
 (eval-and-compile
