a04ce256f3bb9ffd420d65f2ef151a4793f9a200
contrapunctus <contrapunctus@disroot.org>
2023-06-13 06:02:56 +0530

    Add missing file header

--- elisp/jabber-muc-nick-completion.el
+++ elisp/jabber-muc-nick-completion.el
@@ -1,4 +1,26 @@
 ;;; jabber-muc-nick-completion.el --- MUC nick completion  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2008 - Terechkov Evgenii - evg@altlinux.org
+;; Copyright (C) 2007, 2008, 2010 - Kirill A. Korinskiy - catap@catap.ru
+;; Copyright (C) 2007 - Serguei Jidkov - jsv@e-mail.ru
+
+;; This file is a part of jabber.el.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+;;; User customizations here:
 (defcustom jabber-muc-completion-delimiter ": "
   "String to add to end of completion line."
   :type 'string
@@ -163,4 +185,5 @@ OLD is last tried nickname."
 
 (provide 'jabber-muc-nick-completion)
 
+;; arch-tag: 2a81ac72-d261-11dc-be91-000a95c2fcd0
 ;;; jabber-muc-nick-completion.el ends here
