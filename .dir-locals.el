65d524dd2ee36a3359e3f53e027ec2647005b549
contrapunctus <contrapunctus@disroot.org>
2023-06-15 10:21:57 +0530

    Remove old dir-local comment and tags auto-align setup

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,1 @@
-;; `nameless-current-name' cannot be detected in Org mode buffers,
-;; IIRC, and thus has to be defined explicitly. To make use of this,
-;; install `nameless' and enable `nameless-mode' in Org mode buffers.
-((org-mode . ((org-tags-column . -60)
-              (eval . (progn
-                        (make-local-variable 'before-save-hook)
-                        (add-hook 'before-save-hook #'org-align-all-tags nil t))))))
+((org-mode . ((org-tags-column . -60))))
