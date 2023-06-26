c7596f0f20e72bdc90ebaa965335b4a68393647b
Hugh Daschbach <hugh@ccss.com>
2023-06-25 21:17:52 -0700

    Revert emacs-version 24.1 dependency.
    Fix "reference to free variable" warnings in jabber-notifications.el:
    
        Warning: reference to free variable ‘jabber-notifications-icon’
        Warning: reference to free variable ‘jabber-notifications-app’
        Warning: reference to free variable ‘jabber-notifications-timeout’
    
    The warnings are due to the conditional 'require for 'jabber-alert.
    Since ‘jabber.el’ no claims:
    
        Package-Requires: (... (emacs "27.1") ...)
    
    checking for (string< emacs-version "24.1") is counter productive.

--- lisp/jabber-notifications.el
+++ lisp/jabber-notifications.el
@@ -21,66 +21,67 @@
 ;; Built on jabber-libnotify.el.
 
 (eval-when-compile (require 'jabber-alert))
-(unless (string< emacs-version "24.1")  ;notifications.el preset since Emacs 24.1
-  (require 'notifications)
 
-  (defcustom jabber-notifications-icon ""
-    "Icon to be used on the notification pop-up. Default is empty"
-    :type '(file :must-match t)
-    :group 'jabber-alerts)
+(require 'notifications)
 
-  (defcustom jabber-notifications-timeout nil
-    "Specifies the timeout of the pop up window in millisecond"
-    :type 'integer
-    :group 'jabber-alerts)
+(defcustom jabber-notifications-icon ""
+  "Icon to be used on the notification pop-up. Default is empty"
+  :type '(file :must-match t)
+  :group 'jabber-alerts)
 
-  (defcustom jabber-notifications-message-header "Jabber message"
-    "Defines the header of the pop up."
-    :type 'string
-    :group 'jabber-alerts)
+(defcustom jabber-notifications-timeout nil
+  "Specifies the timeout of the pop up window in millisecond"
+  :type 'integer
+  :group 'jabber-alerts)
 
-  (defcustom jabber-notifications-app "Emacs Jabber"
-    "Defines the app of the pop up."
-    :type 'string
-    :group 'jabber-alerts)
+(defcustom jabber-notifications-message-header "Jabber message"
+  "Defines the header of the pop up."
+  :type 'string
+  :group 'jabber-alerts)
 
-  (defcustom jabber-notifications-urgency "low"
-    "Urgency of message"
-    :type '(choice (const :tag "Low" "low")
-                   (const :tag "Normal" "normal")
-                   (const :tag "Critical" "critical"))
-    :group 'jabber-alerts)
+(defcustom jabber-notifications-app "Emacs Jabber"
+  "Defines the app of the pop up."
+  :type 'string
+  :group 'jabber-alerts)
 
-  (defun jabber-message-notifications (from _buffer text title)
-    "Show a message through the notifications.el interface"
-    (let
-        ((body (or (jabber-escape-xml text) " "))
-         (avatar-hash (get (jabber-jid-symbol from) 'avatar-hash)))
-      (notifications-notify
-       :title title
-       :body body
-       :app-icon (or (and avatar-hash (jabber-avatar-find-cached avatar-hash))
-                     jabber-notifications-icon)
-       :app-name jabber-notifications-app
-       :category "jabber.message"
-       :timeout jabber-notifications-timeout)))
+(defcustom jabber-notifications-urgency "low"
+  "Urgency of message"
+  :type '(choice (const :tag "Low" "low")
+                 (const :tag "Normal" "normal")
+                 (const :tag "Critical" "critical"))
+  :group 'jabber-alerts)
 
-  (defun jabber-muc-notifications (nick group buffer text title)
-    "Show MUC message through the notifications.el interface"
-    (jabber-message-notifications group buffer (if nick (format "%s: %s" nick text) text) title)
-    )
+(defun jabber-message-notifications (from _buffer text title)
+  "Show a message through the notifications.el interface"
+  (let
+      ((body (or (jabber-escape-xml text) " "))
+       (avatar-hash (get (jabber-jid-symbol from) 'avatar-hash)))
+    (notifications-notify
+     :title title
+     :body body
+     :app-icon (or (and avatar-hash (jabber-avatar-find-cached avatar-hash))
+                   jabber-notifications-icon)
+     :app-name jabber-notifications-app
+     :category "jabber.message"
+     :timeout jabber-notifications-timeout)))
 
-  (defun jabber-muc-notifications-personal (nick group buffer text title)
-    "Show personal MUC message through the notifications.el interface"
-    (if (jabber-muc-looks-like-personal-p text group)
-        (jabber-muc-notifications nick group buffer text title))
-    )
+(defun jabber-muc-notifications (nick group buffer text title)
+  "Show MUC message through the notifications.el interface"
+  (jabber-message-notifications group buffer (if nick (format "%s: %s" nick text) text) title)
+  )
+
+(defun jabber-muc-notifications-personal (nick group buffer text title)
+  "Show personal MUC message through the notifications.el interface"
+  (if (jabber-muc-looks-like-personal-p text group)
+      (jabber-muc-notifications nick group buffer text title))
+  )
 
   ;; jabber-*-notifications* requires "from" argument, so we cant use
   ;; define-jabber-alert/define-personal-jabber-alert here and do the
   ;; work by hand:
-  (cl-pushnew 'jabber-message-notifications (get 'jabber-alert-message-hooks 'custom-options))
-  (cl-pushnew 'jabber-muc-notifications (get 'jabber-alert-muc-hooks 'custom-options))
-  (cl-pushnew 'jabber-muc-notifications-personal (get 'jabber-alert-muc-hooks 'custom-options)))
+
+(cl-pushnew 'jabber-message-notifications (get 'jabber-alert-message-hooks 'custom-options))
+(cl-pushnew 'jabber-muc-notifications (get 'jabber-alert-muc-hooks 'custom-options))
+(cl-pushnew 'jabber-muc-notifications-personal (get 'jabber-alert-muc-hooks 'custom-options))
 
 (provide 'jabber-notifications)
