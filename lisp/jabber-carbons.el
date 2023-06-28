5258970cecfe8d7545b3cef03a2a991493d414c4
Hugh Daschbach <hugh@ccss.com>
2023-06-28 05:42:19 -0700

    Enable carbons by default.

--- lisp/jabber-carbons.el
+++ lisp/jabber-carbons.el
@@ -7,13 +7,16 @@
 
 (defun jabber-carbon-success (jc xml-data context)
   (when (equal "result" (jabber-xml-get-attribute xml-data 'type))
-    (message "Carbons feature successfully enabled")))
+    (message "Carbons feature successfully enabled for %s"
+             (jabber-connection-jid jc))))
 
 (defun jabber-carbon-failure (jc xml-data context)
   (message "Carbons feature could not be enabled: %S" xml-data))
 
 (add-to-list 'jabber-jid-service-menu
              (cons "Enable Carbons" 'jabber-enable-carbons))
+
+;;;###autoload
 (defun jabber-enable-carbons (jc)
   "Send request to enable XEP-0280 Message Carbons.
 
@@ -25,6 +28,8 @@ JC is the Jabber connection."
                   `(enable ((xmlns . "urn:xmpp:carbons:2")))
                   #'jabber-carbon-success "Carbons feature enablement"
                   #'jabber-carbon-failure "Carbons feature enablement"))
+
+(jabber-disco-advertise-feature "urn:xmpp:carbons:2")
 
 (provide 'jabber-carbons)
 
